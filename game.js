0469fd6f317d341b84185708bdd25b60d1529a66
Josh Lehne <jdlehne@gmail.com>
2017-08-03 19:39:20 -0500

    firebase

--- game.js
+++ game.js
@@ -5,6 +5,18 @@ var startDate = "";
 var totalValue = 0;
 var person = "";
 
+var config = {
+    apiKey: "AIzaSyBk4aeMmuymkDeVoCOq6SLnn7ZYS4ZDo9w",
+    authDomain: "emplyee-test.firebaseapp.com",
+    databaseURL: "https://emplyee-test.firebaseio.com",
+    projectId: "emplyee-test",
+    storageBucket: "",
+    messagingSenderId: "33842781450"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 $("#submitButton").on("click", function(event) {
     event.preventDefault();
     console.log("working");
@@ -17,6 +29,14 @@ $("#submitButton").on("click", function(event) {
     console.log(startDate);
     rate = $("#rateToAdd").val().trim();
     console.log(rate);
+
+       database.ref().set({
+        name: name,
+        role: role,
+        startDate: startDate,
+        rate: rate
+      });
+
 
     person = $("<tr>" + "<th>" + name + "</th>" + "</tr>");
     $("#tableBody").append(person);
